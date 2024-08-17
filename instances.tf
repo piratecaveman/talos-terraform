acd73aa350996e76bf73051efae28006c4dd63d1
piratecaveman <piratecaveman@proton.me>
2024-08-17 22:44:16 +0530

    code refactored

--- instances.tf
+++ instances.tf
@@ -6,3 +6,39 @@ variable "kubenet" {
     "kube-minion-2"
   ]
 }
+
+resource "incus_storage_pool" "openebs" {
+  name   = "openebs"
+  driver = "dir"
+}
+
+resource "incus_storage_volume" "openebs" {
+  count        = length(var.kubenet)
+  name         = var.kubenet[count.index]
+  pool         = incus_storage_pool.openebs.name
+  content_type = "block"
+  config = {
+    size              = "10GB"
+    "security.shared" = true
+  }
+}
+
+resource "incus_instance" "kubenet" {
+  count    = length(var.kubenet)
+  name     = var.kubenet[count.index]
+  type     = "virtual-machine"
+  image    = var.image_name
+  profiles = [var.profile.name]
+  limits = {
+    cpu    = 2
+    memory = "3GB"
+  }
+  device {
+    type = "disk"
+    name = "${var.kubenet[count.index]}-volume"
+    properties = {
+      pool   = incus_storage_pool.openebs.name
+      source = var.kubenet[count.index]
+    }
+  }
+}
